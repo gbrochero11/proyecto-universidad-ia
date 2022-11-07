8fa9f2218e689a3ccb996751fb89bcd23fae7c6d
grbrochero <grbrocherog@ul.edu.co>
2022-11-07 00:14:22 -0500

    fix server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('../dist/iaweb'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join('../dist/iaweb/index.html'));});
+app.listen(process.env.PORT || 8080);
