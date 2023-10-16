386f90c5d4ecfe32c9586dcb20e029bfa02afd16
grbrochero <grbrocherog@ul.edu.co>
2023-10-16 15:59:01 -0500

    Modificaciones en webscraping

--- src/app/modules/webscraping/page/webscraping.component.ts
+++ src/app/modules/webscraping/page/webscraping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-webscraping',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebscrapingComponent implements OnInit {
 
-  constructor() { }
+  URL = 'https://cul-my.sharepoint.com/personal/grbrocherog_ul_edu_co/_layouts/15/embed.aspx?UniqueId=2489cd72-0ef4-459c-b59e-7081613a351c'
+
+  constructor(private spinner: NgxSpinnerService) {}
 
   ngOnInit() {
-  }
+    this.spinner.show();
 
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 5000);
+  }
 }
--- src/app/modules/webscraping/webscraping.module.ts
+++ src/app/modules/webscraping/webscraping.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WebscrapingRoutingModule } from './webscraping-routing.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { WebscrapingComponent } from './page/webscraping.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WebscrapingComponent],
   imports: [
     CommonModule,
-    WebscrapingRoutingModule
-  ]
+    WebscrapingRoutingModule,
+    NgxSpinnerModule
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class WebscrapingModule { }
