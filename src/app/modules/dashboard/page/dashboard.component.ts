40d03edb120c5f34a322badc70d30e2106b6f02f
Gianluca Brochero <38331010+gbrochero11@users.noreply.github.com>
2023-10-16 22:01:48 -0500

    Update dashboard.component.ts

--- src/app/modules/dashboard/page/dashboard.component.ts
+++ src/app/modules/dashboard/page/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService) {}
 
   ngOnInit() {
+    this.spinner.show();
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 5000);
   }
 
 }
