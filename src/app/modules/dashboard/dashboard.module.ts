4aeb1a4c5984f9f74c8523541837b343ad6b9225
Gianluca Brochero <38331010+gbrochero11@users.noreply.github.com>
2023-10-16 22:02:51 -0500

    Update dashboard.module.ts

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './page/dashboard.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    NgxSpinnerModule
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DashboardModule { }
