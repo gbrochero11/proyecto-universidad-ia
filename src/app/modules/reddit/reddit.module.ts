6e797440032784fd47bb2c1fb7219a632e0901b4
grbrochero <grbrocherog@ul.edu.co>
2023-10-16 22:18:56 -0500

    reddit

--- src/app/modules/reddit/reddit.module.ts
+++ src/app/modules/reddit/reddit.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RedditRoutingModule } from './reddit-routing.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { RedditComponent } from './page/reddit.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RedditComponent],
   imports: [
     CommonModule,
-    RedditRoutingModule
-  ]
+    RedditRoutingModule,
+    NgxSpinnerModule
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class RedditModule { }
